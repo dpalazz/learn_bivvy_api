da5c9d9bb664619142f14e28b6a4196ccea182a4
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2018-01-20 14:47:37 -0500

    Services routes completed

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -10,4 +10,36 @@ class LessonsController < ApplicationController
     render json: {status: 200, lesson: lesson}
   end
 
+  def create
+    lesson = Lesson.new(lesson_params)
+    if lesson.save
+      render json: {status: 201, lesson: lesson}
+    else
+      render json: {status: 422, lessson: lesson}
+    end
+  end
+
+  def update
+    lesson = Lesson.find(params[:id])
+    lesson.update(lesson_params)
+    if lesson.save
+      render json: {status: 204, lesson: lesson}
+    else
+      render json: {status: 422, lesson: lesson}
+    end
+  end
+
+  def destroy
+    lesson = Lesson.destroy(params[:id])
+    render status: 204
+  end
+
+  # ****************
+  private
+  # ****************
+
+  def lesson_params
+    params.required(:lesson).permit(:title, :description, :video_url)
+  end
+
 end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,45 @@
 class ServicesController < ApplicationController
+
+  def index
+    services = Service.all
+    render json: {status: 200, services: services}
+  end
+
+  def show
+    service = Service.find(params[:id])
+    render json: {status: 200, service: service}
+  end
+
+  def create
+    service = Service.new(service_params)
+    if service.save
+      render json: {status: 201, service: service}
+    else
+      render json: {status: 422, service: service}
+    end
+  end
+
+  def update
+    service = Service.find(params[:id])
+    service.update(service_params)
+    if service.save
+      render json: {status: 204, service: service}
+    else
+      render json: {status: 422, service: service}
+    end
+  end
+
+  def destroy
+    service = Service.destroy(params[:id])
+    render status: 204
+  end
+
+  # ****************
+  private
+  # ****************
+
+  def service_params
+    params.required(:service).permit(:name)
+  end
+
 end
